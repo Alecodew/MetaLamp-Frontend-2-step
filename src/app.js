2af338220e11b39e6b3683d9dc96473ea0911654
alecodew <alexey.bondarev93.gmail.com>
2021-10-11 19:32:21 +0700

    верстка секции title

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import "./sass/style.sass"; 
 import '../src/img/icons/Group.svg';
+import '../src/img/icons/H1.svg';
+import '../src/img/icons/H2.svg';
+import '../src/img/icons/H3.svg';
+import '../src/img/icons/Body.svg';
+
 
 
